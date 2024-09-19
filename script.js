8f679b746e5c3bdb605112f78524c8a0b44e5231
Luiz Filipe <luizfelipecunha1515@gmail.com>
2024-09-19 08:48:29 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+  const html  = document.documentElement
+  html.classList.toggle('light')
+  
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+ if(html.classList.contains('light')) {
+   // se tiver light mode, adicionar a imagem lignht
+    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito, sorrindo, com oculus, com barba, camisa preta, fundo azul e roxo")
+ } else {
+   // se tiver sem light mode, manter a imagem normal
+   img.setAttribute('src', './assets/avatar.png')
+   img.setAttribute(
+     "alt",
+     "Foto de Mayk Brito, sorrindo, com oculus, com barba, camisa preta e fundo amarelo"
+   )
+
+ }
+
+}
